8725d4eb3c481111770c11fc3f6fe0594a24f219
HueHueberry <hubertfoliente@gmail.com>
2022-05-12 20:35:48 +0800

    Added functions for encrypted sessions Struggling with diffie hellman key exchange, so just stored everything on localstorage

--- static_files/jslib/wordlelib.js
+++ static_files/jslib/wordlelib.js
@@ -1,3 +1,67 @@
+/*
+Added Here so the other file don't need to be imported
+*/
+
+// will be used as key holder for encryption
+var keyHandler = null;
+
+
+/////////////////////////////////
+//    Diffie Encryption part   //
+////////////////////////////////
+// generates a random prime P
+function getRandomPrime() {
+	// local method array
+	prime_list = [5n, 7n, 11n, 13n, 17n, 19n, 23n, 29n, 31n, 37n, 41n, 43n, 47n, 53n, 59n, 61n, 67n, 71n, 73n, 79n, 83n, 89n, 97n]
+	random_index = Math.floor(Math.random() * prime_list.length)
+
+	return prime_list[random_index]
+}
+
+// calculates big integer power with modulo n (if needed)
+function bigIntPow(a, b, n=0n) {
+	output = 1n
+	for (var i = 0; i < b; i++)
+		output *= a
+
+	if (n != 0n)
+		return output % n
+	return output
+}
+
+// generates the P^AmodQ
+function getMixedVal() {
+	const PValue = BigInt(window.localStorage.getItem('P'));
+	const QValue = BigInt(window.localStorage.getItem('Q'));
+	const myValue = BigInt(window.localStorage.getItem('my_val'));
+
+	if (PValue == null) console.error('No PValue found in localstorage');
+	if (QValue == null) console.error('No QValue found in localstorage');
+	if (myValue == null) console.error('No Generated Value found in localstorage');
+
+	return bigIntPow(PValue, myValue, QValue)
+}
+
+// gets the P^BmodQ of the server
+function calculateKey(otherValue) {
+	const QValue = window.localStorage.getItem('Q');
+	const myValue = window.localStorage.getItem('my_val');
+
+	if (QValue == null) console.error('No QValue found in localstorage');
+	if (myValue == null) console.error('No Generated Value found in localstorage');
+
+	return bigIntPow(otherValue, myValue, QValue)
+}
+
+
+// just for diffie value A
+function generateValue() {
+	return Math.ceil( Math.random() * 5000);
+}
+
+////////////////////////////////////////
+//    Modified GET and POST request   //
+////////////////////////////////////////
 function packedRequest_GET(host, callback=(data) => {}) {
 	fetch(host)
 		.then(response => response.text())
@@ -6,7 +70,43 @@ function packedRequest_GET(host, callback=(data) => {}) {
 		});
 }
 
+function packedRequest_POST(host, data, callback=(data) => {}) {
+	fetch(host, {
+		method: 'POST',
+		body: data,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	})
+		.then(response => response.text())
+		.then(data => {
+			callback(data);
+		})
+}
 
+/////////////////////////////
+//   Tokens and Sessions   //
+/////////////////////////////
+// requests for encrypted token (registers the token to the server)
+// used for credential login
+function tokenRequestMulti() {
+	var host = window.location.origin;
+	host += '/encrypted/request/token';
+
+	var currentToken = window.localStorage.getItem('enc_token');
+	if (currentToken === null) {
+		packedRequest_GET(host, (data) => {
+			const tokenPart = data.split(':')[0];
+			const PrimePart = data.split(':')[1];
+
+			window.localStorage.setItem('enc_token', tokenPart);
+			window.localStorage.setItem('P', PrimePart);
+		});
+	}
+}
+
+// requests token for a single player
+// (for keeping track of which game is being played)
 function tokenRequestSingle() {
 	var host = window.location.origin;
 	var modified = host + '/request/stoken/';
@@ -19,8 +119,64 @@ function tokenRequestSingle() {
 	}
 }
 
+// finalize the encryption using this handshake
+function handshake() {
+	const PValue = window.localStorage.getItem('P');
+	var QValue = window.localStorage.getItem('Q');
 
-class InterpreterWordleAPI {
+	// check first if all the parameters are set
+	if (PValue == null) {
+		console.error('Cannot start: Request for encrypted token from the server first!');
+		return;
+	}
+
+	if (QValue == null) {
+		QValue = getRandomPrime();
+		window.localStorage.setItem('Q', QValue);
+	}
+
+	// if this is set, then there's an existing connection for the token
+	var my_val = window.localStorage.getItem('my_val');
+	if (my_val === null) {
+		// generate a new value for our session
+		my_val = generateValue();
+		window.localStorage.setItem('my_val', my_val);
+	}
+
+	// gets the mixed value for client
+	const myMixedValue = getMixedVal();
+
+	// finalizes by registering the token
+	const enc_token = window.localStorage.getItem('enc_token');
+	const host = window.location.origin + '/encrypted/handshake/' + enc_token;
+	var forged_request = `PAQ=${myMixedValue}&Q=${QValue}`;
+
+	packedRequest_POST(host, forged_request, (response) => {
+		console.log('Agreed upon key : ' + response);
+		window.localStorage.setItem('agreed_key', response);
+
+		if (response == 'None')
+			console.error('The tokens in the localstorage is corrupted, it is advised to reset again.');
+	});
+}
+
+
+function flush_enctokens() {
+	window.localStorage.removeItem('enc_token');
+	window.localStorage.removeItem('P');
+	window.localStorage.removeItem('Q');
+	window.localStorage.removeItem('my_val');
+	window.localStorage.removeItem('agreed_key');
+}
+
+function flush_token() {
+	window.localStorage.removeItem('token');
+}
+
+////////////////////
+//   Wordle API   //
+////////////////////
+class SingleWordleAPI {
 	constructor() {
 		this.alphabet = {
 			'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0,
@@ -44,19 +200,15 @@ class InterpreterWordleAPI {
 		}
 	}
 
-	guess(wordGuess) {
+	guess(wordGuess, callback=(arr_data) => {}) {
 		var host = window.location.origin;
 		host += '/single/guess/' + this.token + '/' + wordGuess;
 
-		var output;
   		packedRequest_GET(host, (data) => {
 			this.interpretAPI(wordGuess, data);
-			output = JSON.parse(data);
-
-			console.log(output);
+			const output = JSON.parse(data);
+			callback(output);
 		});
-
-		return output;
 	}
 
 	getAlphabetStatus() {
